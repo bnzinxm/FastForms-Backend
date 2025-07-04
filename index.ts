2345f03f2cb04b00a17435b8ebaf999da55f0f50
bnzinxm <brenobarbosa269@gmail.com>
2025-07-04 11:53:10 -0300

    fix: decreased import lengths

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import * as express from 'express'
-import * as cors from 'cors'
-import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
