953ccd1fd6c0d955668623da471bcc2b20fff890
bnzinxm <brenobarbosa269@gmail.com>
2025-07-04 13:46:55 -0300

    fix: added allowedOrigins to cors

--- index.ts
+++ index.ts
@@ -7,9 +7,19 @@ dotenv.config()
 
 const app = express();
 
+const allowedOrigins = ['https://fastforms-app.vercel.app', 'http://localhost:5173'];
+
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5173/"]
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // caso queira suportar cookies no futuro
 }));
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
