c6ff8c3b5dc23474f95928fcbd28a01aa297929a
bnzinxm <brenobarbosa269@gmail.com>
2025-07-04 14:37:14 -0300

    fix: fixed port

--- config/database.ts
+++ config/database.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
+    port: Number(process.env.DB_PORT) || 33947,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
