d7ddb46b6f6b1a82e9815c9c3d15a9d5f757fa95
bnzinxm <brenobarbosa269@gmail.com>
2025-07-04 12:08:54 -0300

    chore: changed main route

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/v1', (req, res) => {
   res.send('FastForms API rodando com sucesso 🚀')
 })
 
